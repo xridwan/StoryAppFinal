7e8694a700659ba098a6c0a822d731e11a155b7a
xridwan <ridwan.native@gmail.com>
2023-04-03 11:08:18 +0700

    Init

--- app/src/main/java/com/xridwan/mystoryfinal/di/Injection.kt
+++ app/src/main/java/com/xridwan/mystoryfinal/di/Injection.kt
@@ -5,7 +5,6 @@ import com.xridwan.mystoryfinal.data.StoryRepository
 import com.xridwan.mystoryfinal.data.local.StoryDatabase
 import com.xridwan.mystoryfinal.data.network.ApiConfig
 
-// cek
 object Injection {
     fun provideRepository(context: Context): StoryRepository {
         val database = StoryDatabase.getDatabase(context)
